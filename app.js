19af879050b9131ab365bde9baa51a3699caf5f6
Victor Avdeev <avdeevvictor95@gmail.com>
2021-05-23 22:00:22 +0300

    fix: add Port 4000 variable

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
+const Port = 4000;
 
 db.sync();
 app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
+app.listen(Port, function() {
+    console.log(`App is listening on ${Port}`);
 })
