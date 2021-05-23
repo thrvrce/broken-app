c90672509181dbb6cb2a9cada61f9963b141d807
Victor Avdeev <avdeevvictor95@gmail.com>
2021-05-23 22:17:40 +0300

    refactor: replace direct import of validator

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
+var validateSession = require('./middleware/validate-session');
 
 const Port = 4000;
 
 db.sync();
 app.use(express.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(validateSession);
 app.use('/api/game', game);
 app.listen(Port, function() {
     console.log(`App is listening on ${Port}`);
