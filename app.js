53712980f2e74af9af11cf049cd792a8ca660d4d
Victor Avdeev <avdeevvictor95@gmail.com>
2021-05-23 22:05:27 +0300

    fix: delete body-parser, use express.json() instead

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var game = require('./controllers/gamecontroller')
 const Port = 4000;
 
 db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
